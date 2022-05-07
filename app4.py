dfa59d56af8cd552edd1674e74f1df2260b1da8a
Kevin Adrian <adrian.kevin@yahoo.com>
2022-05-07 11:14:09 +0800

    2022/5/7 update

--- app4.py
+++ app4.py
@@ -1,4 +1,5 @@
+#pip install googletrans==3.1.0a0
 from googletrans import Translator
 translator = Translator()
-translation = translator.translate("Der Himmel ist blau und ich mag Bananen", dest='id')
+translation = translator.translate("你好", dest='id')
 print(translation.text)
